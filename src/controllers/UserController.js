65ad3fc18cab656d2195322be955b127d2aad5fd
MinhQuangNgu <quangnm160393@gmail.com>
2022-12-12 12:32:33 +0700

    change

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,7 +8,7 @@ const fetch = require("node-fetch");
 const { OAuth2Client } = require("google-auth-library");
 const Message = require("../models/message");
 
-const url = "https://localhost:3000";
+const url = "https://stphim.xyz";
 
 class UserController {
     async register(req, res) {
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: ["https://stphim.xyz/", "https://www.stphim.xyz/"],
+        origin: "http://103.90.227.181/",
         credentials: true,
     })
 );
